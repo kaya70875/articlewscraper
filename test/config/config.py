7e6dd26bdc892a0e7a845339563730f90a8bf567
kaya70875 <156709255+kaya70875@users.noreply.github.com>
2024-06-08 19:54:46 +0300

    Update config.py

--- test/config/config.py
+++ test/config/config.py
@@ -4,10 +4,8 @@ JSON_FILE_PATH = 'yourfilepath/test/data'
 
 # PAGE VALUES FOR SPIDERS : 
 
-BBC_PAGE = 10
-PSYTODAY_PAGE = 10
-LIVESC_PAGE = 0 # be careful with this there is so many links in one page!
-SCIENCENEWS_PAGE = 50 # this one may require high number.
-NEUROSCIENCE_PAGE = 10
-NYTRAVEL_PAGE = 10
-
+BBC_PAGE = 10 #bbcnews.com
+PSYTODAY_PAGE = 10 #psychologytoday.com/us
+LIVESC_PAGE = 0 # livescience.com / be careful with this there is so many links in one page!
+SCIENCENEWS_PAGE = 50 #sciencenews.com / this one may require high number.
+NEUROSCIENCE_PAGE = 10 #neuroscience.com
