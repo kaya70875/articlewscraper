bb13a16f1dfe4737da4ca76750d0be293ebe5451
kaya70875 <shortsforturkey@gmail.com>
2024-12-27 00:47:59 +0300

    update workflow

--- test/update.py
+++ test/update.py
@@ -1,4 +1,6 @@
 from test.spiders import scienceSpider, newsSpider, psySpider
+from scrapy.utils.project import get_project_settings
+from scrapy.crawler import CrawlerProcess , CrawlerRunner
 import logging
 from colorama import Fore, Style, init
 
@@ -7,9 +9,18 @@ init()
 
 def update():
     print(Fore.GREEN + 'Fetching websites... This could take about 2-15 minutes based on the number of pages being fetched.' + Style.RESET_ALL)
-    scienceSpider.process.start()
-    newsSpider.process.start()
-    psySpider.process.start()
+
+    settings = get_project_settings()
+    runner = CrawlerRunner(settings)
+    runner.crawl(scienceSpider.CrawlLiveScience)
+    runner.crawl(scienceSpider.CrawlScienceNews)
+    d = runner.join()
+
+    from twisted.internet import reactor
+    d.addBoth(lambda _: reactor.stop())
+
+    reactor.run()
+
 if __name__ == '__main__':
     update()
     print(Fore.RED + 'Database updated sucsessfully!' + Style.RESET_ALL)
