3411e5972e524cd66a2d7d9023dd5c2aacf7ca70
kaya70875 <shortsforturkey@gmail.com>
2024-12-27 16:56:47 +0300

    add extra logging to update and pipelines

--- test/test/pipelines.py
+++ test/test/pipelines.py
@@ -1,26 +1,22 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import pymongo
+import logging
 
 class MongoPipeline:
-    def __init__(self, mongo_uri, mongo_db , batch_size=100):
+    def __init__(self, mongo_uri, mongo_db, batch_size=100):
         self.mongo_uri = mongo_uri
         self.mongo_db = mongo_db
         self.batch_size = batch_size
         self.items = []
-    
+        self.inserted_count = 0
+
     @classmethod
-    def from_crawler(cls , crawler):
+    def from_crawler(cls, crawler):
         return cls(
             mongo_uri=crawler.settings.get('MONGO_URI'),
             mongo_db=crawler.settings.get('MONGO_DATABASE')
         )
+
     def open_spider(self, spider):
         self.client = pymongo.MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
@@ -28,12 +24,14 @@ class MongoPipeline:
     def close_spider(self, spider):
         if self.items:
             self.db['sentences'].insert_many(self.items)
+            self.inserted_count += len(self.items)
         self.client.close()
+        logging.info(f'Total sentences inserted: {self.inserted_count}')
 
     def process_item(self, item, spider):
         self.items.append(ItemAdapter(item).asdict())
         if len(self.items) >= self.batch_size:
             self.db['sentences'].insert_many(self.items)
+            self.inserted_count += len(self.items)
             self.items = []
         return item
-
--- test/update.py
+++ test/update.py
@@ -3,12 +3,16 @@ from scrapy.utils.project import get_project_settings
 from scrapy.crawler import CrawlerRunner
 import logging
 from colorama import Fore, Style, init
+import time
+from config.config import BBC_PAGE, PSYTODAY_PAGE, LIVESC_PAGE, SCIENCENEWS_PAGE, NEUROSCIENCE_PAGE
 
 logging.getLogger('scrapy').propagate = False
 init()
 
 def update():
     print(Fore.GREEN + 'Fetching websites... This could take about 2-15 minutes based on the number of pages being fetched.' + Style.RESET_ALL)
+
+    start_time = time.time()
 
     settings = get_project_settings()
     runner = CrawlerRunner(settings)
@@ -25,6 +29,13 @@ def update():
 
     reactor.run()
 
+    end_time = time.time()
+    duration = end_time - start_time
+
+    total_pages = sum([BBC_PAGE, PSYTODAY_PAGE, LIVESC_PAGE, SCIENCENEWS_PAGE, NEUROSCIENCE_PAGE])
+
+    print(Fore.BLUE + f'Database updated successfully in {duration:.2f} seconds!' + Style.RESET_ALL)
+    print(Fore.BLUE + f'Total of {total_pages} pages scraped!' + Style.RESET_ALL)
+
 if __name__ == '__main__':
     update()
-    print(Fore.RED + 'Database updated sucsessfully!' + Style.RESET_ALL)
