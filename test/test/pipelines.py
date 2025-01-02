c6bb41113285d6a61d1d1be974f83ce41ed5891a
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-02 12:50:47 +0300

    add unique index on 'text' field and handle duplicate key errors in MongoPipeline

--- test/test/pipelines.py
+++ test/test/pipelines.py
@@ -2,6 +2,8 @@ from itemadapter import ItemAdapter
 import pymongo
 import logging
 
+import pymongo.errors
+
 class MongoPipeline:
     def __init__(self, mongo_uri, mongo_db, batch_size=100):
         self.mongo_uri = mongo_uri
@@ -21,18 +23,33 @@ class MongoPipeline:
     def open_spider(self, spider):
         self.client = pymongo.MongoClient(self.mongo_uri)
         self.db = self.client[self.mongo_db]
+        #Create a unique index on the 'text' field.
+        self.db['sentences'].create_index([('text' , pymongo.ASCENDING)] , unique=True)
+        logging.info('Created unique index on the text field')
 
     def close_spider(self, spider):
         if self.items:
-            self.db['sentences'].insert_many(self.items)
-            self.inserted_count += len(self.items)
+            try:
+                self.db['sentences'].insert_many(self.items , ordered=False)
+                self.inserted_count += len(self.items)
+            except pymongo.errors.BulkWriteError as e:
+                # Handle duplicate key error
+                for error in e.details['writeErrors']:
+                    if error['code'] == 11000:
+                        logging.warning(f'Duplicate key error: {error["errmsg"]}')
         self.client.close()
         logging.info(f'Total sentences inserted: {self.inserted_count}')
 
     def process_item(self, item, spider):
         self.items.append(ItemAdapter(item).asdict())
         if len(self.items) >= self.batch_size:
-            self.db['sentences'].insert_many(self.items)
-            self.inserted_count += len(self.items)
+            try:
+                self.db['sentences'].insert_many(self.items)
+                self.inserted_count += len(self.items)
+            except pymongo.errors.BulkWriteError as e:
+                # Handle duplicate key error
+                for error in e.details['writeErrors']:
+                    if error['code'] == 11000:
+                        logging.warning(f'Duplicate key error: {error["errmsg"]}')
             self.items = []
         return item
