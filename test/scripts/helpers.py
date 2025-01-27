7c2ea072682b765e513b8c633e74410bd6d7a5f5
Ahmet Kaya <shortsforturkey@gmail.com>
2025-01-27 18:35:46 +0300

    create helper function

---
+++ test/scripts/helpers.py
@@ -0,0 +1,9 @@
+import re
+
+def split_into_sentences(text):
+    # Remove === signs
+    text = re.sub(r'===\s*[^=]+\s*===', '', text)
+    # Improved regex pattern to split text into sentences
+    sentence_endings = re.compile(r'(?<!\b\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s+(?=[A-Z])')
+    sentences = sentence_endings.split(text.strip())
+    return sentences
