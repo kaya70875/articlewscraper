090c2cb7c5ac9cc71998fff6ec129afe09fecdb0
kaya70875 <shortsforturkey@gmail.com>
2026-06-03 21:01:57 +0300

    Update BBCNews spider URLs to remove country parameter and add comments for clarity

--- test/test/spiders/newsSpider.py
+++ test/test/spiders/newsSpider.py
@@ -3,14 +3,22 @@ import json
 import logging
 from config import config
 from .base import BaseArticleSpider
-from scripts.helpers import is_valid_sentence
 
 class BBCNews(BaseArticleSpider):
     name = 'crawlbbcNews'
     start_urls = [
-        'https://web-cdn.api.bbci.co.uk/xd/content-collection/b08a1d2f-6911-4738-825a-767895b8bfc4?country=tr&page=0&size=9',
-        'https://web-cdn.api.bbci.co.uk/xd/content-collection/e2cc1064-8367-4b1e-9fb7-aed170edc48f?country=tr&page=0&size=9',
-        'https://web-cdn.api.bbci.co.uk/xd/content-collection/db5543a3-7985-4b9e-8fe0-2ac6470ea45b?country=tr&page=0&size=9'
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/b08a1d2f-6911-4738-825a-767895b8bfc4?page=0&size=9', # News
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/e2cc1064-8367-4b1e-9fb7-aed170edc48f?page=0&size=9', # News
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/092c7c94-aa9b-4933-9349-eb942b3bde77?page=0&size=9', # Technology
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/98529df5-2749-4618-844f-96431b3084d9?page=0&size=9', # Travel
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/6ddbca4a-80f3-4875-8dd7-cbe76fca05b8?page=0&size=9' # Travel
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/daa2a2f9-0c9e-4249-8234-bae58f372d82?page=0&size=9', # Business
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/3da03ce0-ee41-4427-a5d9-1294491e0448?page=0&size=9', # Innovation
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/6d50eb9d-ee20-40fe-8e0f-f506d6a02b78?page=0&size=9', # Culture
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/ef20229c-cde4-449f-b225-6db94953d2ce?page=0&size=9', # Arts
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/9f0b9075-b620-4859-abdc-ed042dd9ee66?page=0&size=9', # Earth
+        'https://web-cdn.api.bbci.co.uk/xd/content-collection/6d032332-6ce5-425b-85a6-f260355718b3?page=0&size=9', # AI News
+
     ]
 
     custom_settings = {
@@ -46,7 +54,7 @@ class BBCNews(BaseArticleSpider):
                 yield scrapy.Request(next_url, callback=self.parse_article)
 
             next_page = current_page + 1
-            next_page_url = f'{base_url}?country=tr&page={next_page}&size=9'
+            next_page_url = f'{base_url}?page={next_page}&size=9'
             if next_page_url:
                 yield scrapy.Request(next_page_url, callback=self.parse, meta={'page': next_page, 'base_url': base_url})
 
